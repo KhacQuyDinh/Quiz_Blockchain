e925ec423053e7577a6da59cff4e58403ecd03ad
lumosnysm <lumosnysm@gmail.com>
2018-04-10 17:17:45 +0700

    Update QuizContract.sol

--- coursetro-eth/QuizContract.sol
+++ coursetro-eth/QuizContract.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.4.21;
 
 contract Quiz {
+    //may need to change if increase the total supply.
 	uint256 priceEachQuiz = 1000000000000000;
+
     
 	//remember string is costly, so we can change it to bytes,...
-	function Quiz() public {	    	    
+	function Quiz() public {
+	    //init total supply = an amount of free given money.
         initQuizDb();
 	}
 	
@@ -41,7 +44,7 @@ contract Quiz {
 	    , 'Everything be smart and self thinking', 'Just like the third revolution', 'Use computer', 'Use a motorbike', '', 1, 0));
 
 	}
-
+	
 	struct quiz_pattern {
 	    uint256 id;
 	    bytes question;
@@ -54,6 +57,8 @@ contract Quiz {
 	    uint256 startingTime;
 	}
 	
+	//like a database.
+	//mapping (uint256 => quiz_pattern) public quiz_set;
 	quiz_pattern[] public quiz_set;
 	
  	uint256 current_quiz_id = 0;
@@ -85,15 +90,11 @@ contract Quiz {
 		bytes answer_C,
 		bytes answer_D
 	);
-	
+
 	event update_money_evt(
 	    bool isSpend,
 		address player,
 		uint256 token
-	);
-	
-	event update_quiz_closed_evt( 
-		uint256 startingTime
 	);
 	
 	//set new 
@@ -101,12 +102,14 @@ contract Quiz {
 	    bool isRight = false;
 		if (quiz_set[quiz_id].answer_check_id == user_answer_id) {
 		    isRight = true;
-		    //increase awardMoney
-		    emit update_money_evt(false, msg.sender, priceEachQuiz * 2);
 		    current_general_num_right_answer += 1;
+		    //increase two coin if get the correct answer.
+		    emit update_money_evt(false, msg.sender, priceEachQuiz * 2);
 		} else {
 		    isRight = false;
-		    current_general_num_false_answer += 1;		    
+		    current_general_num_false_answer += 1;
+		    //decrease one coin if get the wrong answer.
+		    //balanceOf[msg.sender] -= 1;
 		}
 
        // call event to update info in gui.
@@ -118,60 +121,37 @@ contract Quiz {
         , quiz_set[quiz_id].answer_check
         , user_answer_id
         //the rule of index in programming.
-        , current_quiz_id - 1);
+        , current_quiz_id - 1
+        );
 	}
 
 	function getTheNextQuiz(uint256 time) public {
-		require(default_total_quiz - current_quiz_id > 0 
+	    
+	    require(default_total_quiz - current_quiz_id > 0 
 	    && msg.sender.balance >= priceEachQuiz);
 	    
-	    quiz_pattern memory quiz = quiz_set[current_quiz_id];
-	    current_quiz_id += 1;	    	
-	    
+	    quiz_pattern storage quiz = quiz_set[current_quiz_id];
+	    current_quiz_id += 1;	    
 	    //update startingTime
 	    quiz.startingTime = time;
 	   
-	   	//decrease awardMoney
-		emit update_money_evt(true, msg.sender, priceEachQuiz);
-		
-	    //will decrease ether.
-		emit update_the_next_quiz_evt(
-		  current_general_num_right_answer
-		, current_general_num_false_answer
-		, default_total_quiz
-		, quiz.id
-		, quiz.question
-		, quiz.answer_A
-		, quiz.answer_B
-		, quiz.answer_C
-		, quiz.answer_D);
-	}
-	
-	//get the quiz by its id.
-	function getTheNextQuizById(uint256 quiz_id) public {
-	        
-	    quiz_pattern storage quiz = quiz_set[quiz_id];
-	    
-		emit update_the_next_quiz_evt(
-		  current_general_num_right_answer
-		, current_general_num_false_answer
-		, default_total_quiz
-		, quiz.id
-		, quiz.question
-		, quiz.answer_A
-		, quiz.answer_B
-		, quiz.answer_C
-		, quiz.answer_D);
-		
-		emit update_quiz_closed_evt(quiz.startingTime);
-	}
-	
-	// call this method when timer count to 00:00  , update the submit button to next button
-	function messageToServerWhenTimeOut(uint256 quiz_id) view public {
-	    quiz_pattern storage quiz = quiz_set[quiz_id];
-	    emit update_quiz_closed_evt(quiz.startingTime);
-	}
+	   emit update_money_evt(true, msg.sender, priceEachQuiz);
 
+		emit update_the_next_quiz_evt(
+		  current_general_num_right_answer
+		, current_general_num_false_answer
+		, default_total_quiz
+		, quiz.id
+		, quiz.question
+		, quiz.answer_A
+		, quiz.answer_B
+		, quiz.answer_C
+		, quiz.answer_D);
+	}
+	
+
+
+	// useless
 	function getQuizStartingTimeById(uint256 quiz_id) public view returns(uint256) {
 	    return quiz_set[quiz_id].startingTime;
 	}
