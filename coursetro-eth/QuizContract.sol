ef08899a44ec907b99949ce01befa94b79280477
moduledk <moduledk@gmail.com>
2018-04-10 13:23:22 +0700

    remix-live

--- coursetro-eth/QuizContract.sol
+++ coursetro-eth/QuizContract.sol
@@ -1,13 +1,10 @@
 pragma solidity ^0.4.21;
 
 contract Quiz {
-    //may need to change if increase the total supply.
-    mapping (address => uint256) public balanceOf;
+	uint256 priceEachQuiz = 1000000000000000;
     
 	//remember string is costly, so we can change it to bytes,...
-	function Quiz() public {
-	    //init total supply = an amount of free given money.
-	    balanceOf[msg.sender] = 100;
+	function Quiz() public {	    	    
         initQuizDb();
 	}
 	
@@ -44,7 +41,7 @@ contract Quiz {
 	    , 'Everything be smart and self thinking', 'Just like the third revolution', 'Use computer', 'Use a motorbike', '', 1, 0));
 
 	}
-	
+
 	struct quiz_pattern {
 	    uint256 id;
 	    bytes question;
@@ -57,8 +54,6 @@ contract Quiz {
 	    uint256 startingTime;
 	}
 	
-	//like a database.
-	//mapping (uint256 => quiz_pattern) public quiz_set;
 	quiz_pattern[] public quiz_set;
 	
  	uint256 current_quiz_id = 0;
@@ -74,8 +69,7 @@ contract Quiz {
 		uint256 total_quiz,
 		bytes answer_check,
 		uint256 user_answer_id,
-		uint256 server_current_quiz_id,
-		uint256 balance
+		uint256 server_current_quiz_id
 	);
 	
 	
@@ -92,19 +86,27 @@ contract Quiz {
 		bytes answer_D
 	);
 	
+	event update_money_evt(
+	    bool isSpend,
+		address player,
+		uint256 token
+	);
+	
+	event update_quiz_closed_evt( 
+		uint256 startingTime
+	);
+	
 	//set new 
 	function submitAnswer(uint256 quiz_id, uint256 user_answer_id) public {
 	    bool isRight = false;
 		if (quiz_set[quiz_id].answer_check_id == user_answer_id) {
 		    isRight = true;
+		    //increase awardMoney
+		    emit update_money_evt(false, msg.sender, priceEachQuiz * 2);
 		    current_general_num_right_answer += 1;
-		    //increase two coin if get the correct answer.
-		    balanceOf[msg.sender] += 2;
 		} else {
 		    isRight = false;
-		    current_general_num_false_answer += 1;
-		    //decrease one coin if get the wrong answer.
-		    //balanceOf[msg.sender] -= 1;
+		    current_general_num_false_answer += 1;		    
 		}
 
        // call event to update info in gui.
@@ -116,23 +118,23 @@ contract Quiz {
         , quiz_set[quiz_id].answer_check
         , user_answer_id
         //the rule of index in programming.
-        , current_quiz_id - 1
-        , balanceOf[msg.sender]);
+        , current_quiz_id - 1);
 	}
 
 	function getTheNextQuiz(uint256 time) public {
+		require(default_total_quiz - current_quiz_id > 0 
+	    && msg.sender.balance >= priceEachQuiz);
 	    
-	    require(default_total_quiz - current_quiz_id > 0 
-	    && balanceOf[msg.sender] > 0);
-	    
-	    quiz_pattern storage quiz = quiz_set[current_quiz_id];
-	    current_quiz_id += 1;
-	    //spend one coin on geting the next quiz.
-	    balanceOf[msg.sender] -= 1;
+	    quiz_pattern memory quiz = quiz_set[current_quiz_id];
+	    current_quiz_id += 1;	    	
 	    
 	    //update startingTime
 	    quiz.startingTime = time;
 	   
+	   	//decrease awardMoney
+		emit update_money_evt(true, msg.sender, priceEachQuiz);
+		
+	    //will decrease ether.
 		emit update_the_next_quiz_evt(
 		  current_general_num_right_answer
 		, current_general_num_false_answer
@@ -145,9 +147,31 @@ contract Quiz {
 		, quiz.answer_D);
 	}
 	
+	//get the quiz by its id.
+	function getTheNextQuizById(uint256 quiz_id) public {
+	        
+	    quiz_pattern storage quiz = quiz_set[quiz_id];
+	    
+		emit update_the_next_quiz_evt(
+		  current_general_num_right_answer
+		, current_general_num_false_answer
+		, default_total_quiz
+		, quiz.id
+		, quiz.question
+		, quiz.answer_A
+		, quiz.answer_B
+		, quiz.answer_C
+		, quiz.answer_D);
+		
+		emit update_quiz_closed_evt(quiz.startingTime);
+	}
+	
+	// call this method when timer count to 00:00  , update the submit button to next button
+	function messageToServerWhenTimeOut(uint256 quiz_id) view public {
+	    quiz_pattern storage quiz = quiz_set[quiz_id];
+	    emit update_quiz_closed_evt(quiz.startingTime);
+	}
 
-
-	// useless
 	function getQuizStartingTimeById(uint256 quiz_id) public view returns(uint256) {
 	    return quiz_set[quiz_id].startingTime;
 	}
@@ -167,6 +191,6 @@ contract Quiz {
 	}
 	
 	function getCurrentBalance() public view returns(uint256) {
-	    return balanceOf[msg.sender];
+		return msg.sender.balance;
 	}
 }
