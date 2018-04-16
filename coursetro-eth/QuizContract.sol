2d9f574ddfb5d6618be555efac8d72120bd37ec9
moduledk <moduledk@gmail.com>
2018-04-16 18:59:01 +0700

    enhance and fix bug in issue

--- coursetro-eth/QuizContract.sol
+++ coursetro-eth/QuizContract.sol
@@ -13,34 +13,34 @@ contract Quiz {
 	function initQuizDb() public {
 	    //init 
 	    quiz_set.push(quiz_pattern(0, 'When we use "hello"?'
-	    , 'For the meeting someone', 'Feel sad', 'Feel fun', 'Feel bored', '', 1, 0));
+	    , 'For the meeting someone', 'Feel sad', 'Feel fun', 'Feel bored', '', 1));
 
 	    quiz_set.push(quiz_pattern(1, 'What is pig?'
-	    , 'River', 'Animal', 'Tree', 'A kind of worm', '', 2, 0));
+	    , 'River', 'Animal', 'Tree', 'A kind of worm', '', 2));
 
 	     quiz_set.push(quiz_pattern(2, 'What is clever?'
-	    , 'Skills in reality', 'Just like intelligent ', 'Skills but cannot use', 'Silly', '', 1, 0));
+	    , 'Skills in reality', 'Just like intelligent ', 'Skills but cannot use', 'Silly', '', 1));
 
 	     quiz_set.push(quiz_pattern(3, 'Who is Washington?'
-	    , 'The first president of India', 'The first president of England', 'The first president of America', 'The first president of Cubai', '', 3, 0));
+	    , 'The first president of India', 'The first president of England', 'The first president of America', 'The first president of Cubai', '', 3));
 
 	     quiz_set.push(quiz_pattern(4, 'What is DellInspiron 3552?'
-	    , 'A book', 'A desk', 'A chair', 'A computer', '', 4, 0));
+	    , 'A book', 'A desk', 'A chair', 'A computer', '', 4));
 
 	     quiz_set.push(quiz_pattern(5, 'Who is the president of Vietnam in 2018?'
-	    , 'Mr. Truong Tan Sang', 'Mrs. Nguyen Thi Kim Ngan', 'Mr. Vu Duc Dam', 'Mrs. Phung Thi Tien', '', 1, 0));
+	    , 'Mr. Truong Tan Sang', 'Mrs. Nguyen Thi Kim Ngan', 'Mr. Vu Duc Dam', 'Mrs. Phung Thi Tien', '', 1));
 
 	     quiz_set.push(quiz_pattern(6, 'Which word is adjective?'
-	    , 'Bored', 'Action', 'Cut', 'Slowly', '', 1, 0));
+	    , 'Bored', 'Action', 'Cut', 'Slowly', '', 1));
 	   
 	     quiz_set.push(quiz_pattern(7, 'What should use do when you feel bored?'
-	    , 'Relax', 'Work hard', 'Hear something bad', 'Hear something boring', '', 1, 0));
+	    , 'Relax', 'Work hard', 'Hear something bad', 'Hear something boring', '', 1));
 	   
       	 quiz_set.push(quiz_pattern(8, 'Who is Thomas Edison?'
-	    , 'Who invented many good things', 'A psychology', 'A kind of Internet', 'A kind of cat', '', 1, 0));
+	    , 'Who invented many good things', 'A psychology', 'A kind of Internet', 'A kind of cat', '', 1));
 
 	     quiz_set.push(quiz_pattern(9, 'What is the fourth industrial revolution?'
-	    , 'Everything be smart and self thinking', 'Just like the third revolution', 'Use computer', 'Use a motorbike', '', 1, 0));
+	    , 'Everything be smart and self thinking', 'Just like the third revolution', 'Use computer', 'Use a motorbike', '', 1));
 	}
 
 	struct quiz_pattern {
@@ -52,7 +52,6 @@ contract Quiz {
 	    bytes answer_D;
 	    bytes answer_check;
 	    uint256 answer_check_id;
-	    uint256 startingTime;
 	}
 	
 	struct user_storage_pattern {
@@ -62,6 +61,7 @@ contract Quiz {
 	   uint256 user_num_wrong_answer;
 	   uint256 user_total_answer;
 	   uint256 user_quiz_starting_time;
+	   bool isAnswered;
 	}
 	
 	mapping (address => user_storage_pattern) public userStorage;
@@ -72,11 +72,12 @@ contract Quiz {
 	function initServerUserStorage() public {
 	   if (userStorage[msg.sender].isInit == false) {
 	     userStorage[msg.sender].isInit = true;     
-	     userStorage[msg.sender].user_quiz_id = default_total_quiz;  
+	     userStorage[msg.sender].user_quiz_id = default_total_quiz + 1;  
 	     userStorage[msg.sender].user_num_correct_answer = 0;     
 	     userStorage[msg.sender].user_num_wrong_answer = 0;     
 	     userStorage[msg.sender].user_total_answer = 0;     
 	     userStorage[msg.sender].user_quiz_starting_time = 0;
+	     userStorage[msg.sender].isAnswered = false;
 	   } 
 	}
 	
@@ -108,7 +109,33 @@ contract Quiz {
 	function getServerUserQuizStartingTime() public view returns(uint256) {
 	    return userStorage[msg.sender].user_quiz_starting_time;
 	}
-
+	
+	function getServerUserQuizIsAnswered() public view returns(bool) {
+	    return userStorage[msg.sender].isAnswered;
+	}
+	
+	function getServerCurrentQuizId() public view returns(uint256) {
+	    //because current_quiz_id increased one uint256 after getTheNextQuiz() is called.
+	    if (current_quiz_id > 0) {
+	        return current_quiz_id - 1;
+	    } else {
+	        return default_total_quiz + 1;
+	    }
+	}
+	
+	function getServerDefaultTotalQuiz() public view returns(uint256) {
+	    return default_total_quiz;
+	}
+	
+	function getServerUserWalletBalance() public view returns(uint256) {
+		return msg.sender.balance;
+	}
+	
+	function isServerCloseGame() public view returns(bool) {
+	    //the game happens in 3h
+	    return  now - openTimeContract > 3600*3;
+	}
+	
 	quiz_pattern[] public quiz_set;
 	
  	uint256 current_quiz_id = 0;
@@ -142,6 +169,16 @@ contract Quiz {
 		bytes answer_D
 	);
 	
+	//call event to update the next question in gui.
+	event update_the_old_quiz_evt(
+		uint256 quiz_id,
+		bytes question,
+		bytes answer_A,
+		bytes answer_B,
+		bytes answer_C,
+		bytes answer_D
+	);
+	
 	event update_money_evt(
 	    bool isSpend,
 		address player,
@@ -152,15 +189,19 @@ contract Quiz {
 	function submitAnswer2Server(uint256 quiz_id, uint256 user_answer_id) public {
 	    require(isServerCloseGame() == false);
 	    
+	    userStorage[msg.sender].isAnswered = true;
+	    
 	    bool isRight = false;
 		if (quiz_set[quiz_id].answer_check_id == user_answer_id) {
 		    isRight = true;
 		    //increase awardMoney
 		    emit update_money_evt(false, msg.sender, priceEachQuiz * 2);
 		    current_general_num_right_answer += 1;
+		    userStorage[msg.sender].user_num_correct_answer += 1;
 		} else {
 		    isRight = false;
-		    current_general_num_false_answer += 1;		    
+		    current_general_num_false_answer += 1;	
+		    userStorage[msg.sender].user_num_wrong_answer += 1;
 		}
 
        // call event to update info in gui.
@@ -175,11 +216,17 @@ contract Quiz {
 	    && msg.sender.balance >= priceEachQuiz 
 	    && isServerCloseGame() == false);
 	    
-	    quiz_pattern memory quiz = quiz_set[current_quiz_id];
-	    current_quiz_id += 1;	    	
+	    userStorage[msg.sender].isAnswered = false;
+	    
+	    //update curent_quiz_id for the quiz
+	    userStorage[msg.sender].user_quiz_id = current_quiz_id;
+	    
+	    quiz_pattern storage quiz = quiz_set[current_quiz_id];
 	    
 	    //update startingTime
-	    quiz.startingTime = now;
+	    userStorage[msg.sender].user_quiz_starting_time = now;
+	    
+	    current_quiz_id += 1;	    	
 	   
 	   	//decrease awardMoney
 		emit update_money_evt(true, msg.sender, priceEachQuiz);
@@ -195,43 +242,16 @@ contract Quiz {
 	}
 	
 	//get the quiz by its id.
-	function getServerTheNextQuizById(uint256 quiz_id) public {
+	function getServerTheOldQuizById(uint256 quiz_id) public {
 	        
 	    quiz_pattern storage quiz = quiz_set[quiz_id];
 	    
-		emit update_the_next_quiz_evt(
+		emit update_the_old_quiz_evt(
 		  quiz.id
 		, quiz.question
 		, quiz.answer_A
 		, quiz.answer_B
 		, quiz.answer_C
 		, quiz.answer_D);			
-	}
-
-	function getServerQuizStartingTimeById(uint256 quiz_id) public view returns(uint256) {
-	    return quiz_set[quiz_id].startingTime;
-	}
-	
-	
-	function getServerCurrentQuizId() public view returns(uint256) {
-	    //because current_quiz_id increased one uint256 after getTheNextQuiz() is called.
-	    if (current_quiz_id > 0) {
-	        return current_quiz_id - 1;
-	    } else {
-	        return default_total_quiz;
-	    }
-	}
-	
-	function getServerDefaultTotalQuiz() public view returns(uint256) {
-	    return default_total_quiz;
-	}
-	
-	function getServerUserWalletBalance() public view returns(uint256) {
-		return msg.sender.balance;
-	}
-	
-	function isServerCloseGame() public view returns(bool) {
-	    //the game happens in 3h
-	    return (current_quiz_id == default_total_quiz + 1) || (now - openTimeContract > 3600*3);
 	}
 }
